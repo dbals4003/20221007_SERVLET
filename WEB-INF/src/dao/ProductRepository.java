e865325f5ffa343c7526c71687555383937a1b7a
이유민 <dbals4003@sungkyul.ac.kr>
2023-11-30 12:04:29 +0000

    자바웹프로그래밍(2) 6주차 실습

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -17,7 +17,7 @@ public class ProductRepository {
         phone.setManufacturer("NVIDIA");
         phone.setUnitsInStock(1000);
         phone.setCondition("New");
-        phone.setFilename("G4080.png");
+        phone.setFilename("G4080.jpg");
         
         Product notebook = new Product("G4070", "PNY Geforce RTX 4070", 800000);
         notebook.setDescription("GDDR6X(DDR6X), 12GB, 247.4mm");
@@ -25,7 +25,7 @@ public class ProductRepository {
         notebook.setManufacturer("NVIDIA");
         notebook.setUnitsInStock(1000);
         notebook.setCondition("Refurbished");
-        notebook.setFilename("G4070.png");
+        notebook.setFilename("G4070.jpg");
         
         Product tablet = new Product("G4060", "GIGABYTE Geforce RTX 4060", 400000);
         tablet.setDescription("GDDR6(DDR6), 8GB, 192mm");
@@ -33,15 +33,15 @@ public class ProductRepository {
         tablet.setManufacturer("NVIDIA");
         tablet.setUnitsInStock(1000);
         tablet.setCondition("Refurbished");
-        tablet.setFilename("G4060.png");
+        tablet.setFilename("G4060.jpg");
         
-        Product VGA1 = new Product("G3080", "MSI Geforce RTX 3080", 1000000);
+        Product VGA1 = new Product("G3080", "MSI Geforce RTX 3080Ti", 1000000);
         VGA1.setDescription("GDDR6X(DDR6X), 12GB, 317.8mm");
         VGA1.setCategory("VGA");
         VGA1.setManufacturer("NVIDIA");
         VGA1.setUnitsInStock(1000);
         VGA1.setCondition("New");
-        VGA1.setFilename("G3080.png");
+        VGA1.setFilename("G3080Ti.jpg");
         
         Product VGA2 = new Product("G3070", "GIGABYTE Geforce RTX 3070", 800000);
         VGA2.setDescription("GDDR6(DDR6), 8GB, 286mm");
@@ -49,7 +49,7 @@ public class ProductRepository {
         VGA2.setManufacturer("NVIDIA");
         VGA2.setUnitsInStock(1000);
         VGA2.setCondition("Old");
-        VGA2.setFilename("G3070.png");
+        VGA2.setFilename("G3070.jpg");
         
         Product VGA3 = new Product("G3060", "MANLI Geforce RTX 3060", 400000);
         VGA3.setDescription("GDDR6(DDR6), 12GB, 245mm");
@@ -57,7 +57,7 @@ public class ProductRepository {
         VGA3.setManufacturer("NVIDIA");
         VGA3.setUnitsInStock(1000);
         VGA3.setCondition("Old");
-        VGA3.setFilename("G3060.png");
+        VGA3.setFilename("G3060.jpg");
         
         Product VGA4 = new Product("G1660", "ZOTAC Geforce GTX 1660", 400000);
         VGA4.setDescription("GDDR5(DDR5), 6GB, 214mm");
@@ -65,7 +65,7 @@ public class ProductRepository {
         VGA4.setManufacturer("NVIDIA");
         VGA4.setUnitsInStock(1000);
         VGA4.setCondition("Old");
-        VGA4.setFilename("G1660.png");
+        VGA4.setFilename("G1660.jpg");
         
         Product VGA5 = new Product("G1650", "ASUS Geforce GTX 1650", 200000);
         VGA5.setDescription("GDDR5(DDR5), 4GB, 204mm");
@@ -73,7 +73,7 @@ public class ProductRepository {
         VGA5.setManufacturer("NVIDIA");
         VGA5.setUnitsInStock(1000);
         VGA5.setCondition("Old");
-        VGA5.setFilename("G1650.png");
+        VGA5.setFilename("G1650.jpg");
         
         Product VGA6 = new Product("G1060", "GIGABYTE Geforce GTX 1060", 200000);
         VGA6.setDescription("GDDR5(DDR5), 2GB, 200mm");
@@ -81,7 +81,7 @@ public class ProductRepository {
         VGA6.setManufacturer("NVIDIA");
         VGA6.setUnitsInStock(1000);
         VGA6.setCondition("Old");
-        VGA6.setFilename("G1060.png");
+        VGA6.setFilename("G1060.jpg");
         
         // 위와 같이 상품 초기화 하고 아래에 상품을 추가
         
